49c994cb23e09ddf8c4a499a2f79e2b3a0f8a1fc
dogan can selek <dogancan.selek@for.com.tr>
2022-12-22 23:41:09 +0300

    update main for adding pages

--- src/App.js
+++ src/App.js
@@ -1,37 +1,19 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import Layout from './components/Layout';
 import './App.css';
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="thanks" element={<></>} />
+        </Route>
       </Routes>
     </div>
   );
 }
 
 export default App;
-
-function Home() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        Home
-      </h1>
-      <button className='p-4 bg-black text-white'><Link to="/about">About</Link></button>
-    </div>
-  );
-}
-
-function About() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-        About
-      </h1>
-      <button className='p-4 bg-black text-white'><Link to="/">go back</Link></button>
-    </div>
-  );
-}
