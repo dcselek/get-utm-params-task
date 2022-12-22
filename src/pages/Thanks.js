fe153a382bc795a5b2c6727778cb773cb0973ea5
dogan can selek <canselek2434@gmail.com>
2022-12-23 02:13:46 +0300

    update Thanks

--- src/pages/Thanks.js
+++ src/pages/Thanks.js
@@ -1,18 +1,15 @@
 import React, { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom'
+import { useSearchParams } from 'react-router-dom';
 
 function Thanks() {
-    const location = useLocation()
-    const [navState, setNavState] = useState(null)
+    const [searchParams] = useSearchParams();
+    const [formParams, setFormParams] = useState(null)
 
     useEffect(() => {
-        const { state } = location
-        //if state is not defined, redirect to home page
-        console.log(state)
-        if (!state) {
-            window.location.href = '/'
-        }
-        setNavState(state)
+        let fullName = searchParams.get("fullName");
+        let email = searchParams.get("email");
+        let phone = searchParams.get("phone");
+        setFormParams({ fullName, email, phone })
     }, [])
 
     const clearLocalStorage = () => {
@@ -24,11 +21,11 @@ function Thanks() {
         <div className='flex w-full h-screen items-center justify-center flex-col gap-8'>
             <h1 className='text-4xl text-yellow-500'>Thanks for submitting the form</h1>
             <p className='text-yellow-200'>We created localStorage keys for your UTM params <br/> and <br/> published your form data under this paragraph:</p>
-            {navState && (
+            {formParams && (
                 <div className='flex text-white flex-col font-bold items-center'>
-                    <p>{navState.fullName}</p>
-                    <p>{navState.email}</p>
-                    <p>{navState.phone}</p>
+                    <p>{formParams.fullName}</p>
+                    <p>{formParams.email}</p>
+                    <p>{formParams.phone}</p>
                 </div>
             )}
             <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline' onClick={clearLocalStorage}>
