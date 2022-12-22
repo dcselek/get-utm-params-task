4cdd46d23e77fe5fca0cd137d53c636b3c993611
dogan can selek <dogancan.selek@for.com.tr>
2022-12-22 23:41:28 +0300

    add homa page

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Form from '../components/Form/index'
+
+function HomePage() {
+  return (
+    <div className='flex w-full h-screen items-center justify-center'>
+      <Form />
+    </div>
+  )
+}
+
+export default HomePage
