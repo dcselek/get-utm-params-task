603ec159f4514c84e6df404c850b48268a0d6b04
dogan can selek <canselek2434@gmail.com>
2022-12-23 02:17:24 +0300

    add loading

--- src/pages/Thanks.js
+++ src/pages/Thanks.js
@@ -17,22 +17,30 @@ function Thanks() {
         window.location.href = '/'
     }
 
-    return (
-        <div className='flex w-full h-screen items-center justify-center flex-col gap-8'>
-            <h1 className='text-4xl text-yellow-500'>Thanks for submitting the form</h1>
-            <p className='text-yellow-200'>We created localStorage keys for your UTM params <br/> and <br/> published your form data under this paragraph:</p>
-            {formParams && (
-                <div className='flex text-white flex-col font-bold items-center'>
-                    <p>{formParams.fullName}</p>
-                    <p>{formParams.email}</p>
-                    <p>{formParams.phone}</p>
-                </div>
-            )}
-            <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline' onClick={clearLocalStorage}>
-                Go to home page and clear localStorage
-            </button>
-        </div>
-    )
+    if (!formParams) {
+        return (
+            <div className='flex w-full h-screen items-center justify-center flex-col gap-8'>
+                <h1 className='text-4xl text-yellow-500'>Loading</h1>
+            </div>
+        )
+    } else {
+        return (
+            <div className='flex w-full h-screen items-center justify-center flex-col gap-8'>
+                <h1 className='text-4xl text-yellow-500'>Thanks for submitting the form</h1>
+                <p className='text-yellow-200'>We created localStorage keys for your UTM params <br /> and <br /> published your form data under this paragraph:</p>
+                {formParams && (
+                    <div className='flex text-white flex-col font-bold items-center'>
+                        <p>{formParams.fullName}</p>
+                        <p>{formParams.email}</p>
+                        <p>{formParams.phone}</p>
+                    </div>
+                )}
+                <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline' onClick={clearLocalStorage}>
+                    Go to home page and clear localStorage
+                </button>
+            </div>
+        )
+    }
 }
 
 export default Thanks
